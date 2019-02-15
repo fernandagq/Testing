e66584f045ccbc0bd20dce0c7a1e4e7ca1a3c703
Fer <fgarcia@mail.wm.edu>
2019-02-15 12:22:13 -0500

    Updates

---
+++ precache-manifest.cf4ba2f086995027e18f487c039209d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ada2fb1769b8d5c61a0",
+    "url": "/Testing/static/css/main.3563c477.chunk.css"
+  },
+  {
+    "revision": "2ada2fb1769b8d5c61a0",
+    "url": "/Testing/static/js/main.2ada2fb1.chunk.js"
+  },
+  {
+    "revision": "9bee6f854cf4d8cd3777",
+    "url": "/Testing/static/js/runtime~main.9bee6f85.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/Testing/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Testing/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3443c628e4d06f195722ece8a69b8d83",
+    "url": "/Testing/index.html"
+  }
+];
